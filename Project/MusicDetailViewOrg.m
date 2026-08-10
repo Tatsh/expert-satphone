e72381c7a45cfc493debdf3c038b232fc6413c36
Andrew Udvare <audvare@gmail.com>
2026-08-10 19:41:43 +0000

    musicDetailViewRpl, musicDetailViewOrg: reconstruct three more methods each
    Continue Project/MusicDetailViewRpl.m and Project/MusicDetailViewOrg.m, each
    verified against the disassembly (both identical to their MusicDetailViewKnt
    counterparts):
    
    - pushButtonUpload: forwards to uploadStart when uploading is allowed.
    - loadListRelease detaches and drops the edit-file list controller.
    - popoverPresentationControllerDidDismissPopover: releases the list and
      re-enables the cover tap.
    
    rctool audit addresses: 0 mismatched methods, 0 mismatched constants. STATUS
    regenerated (Objective-C 4726/4987).
    
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- Project/MusicDetailViewOrg.m
+++ Project/MusicDetailViewOrg.m
@@ -2,6 +2,8 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+#import "MusicSelectViewController.h"
+
 @implementation MusicDetailViewOrg
 
 /** @ghidraAddress 0x502bc */
@@ -12,6 +14,26 @@
 /** @ghidraAddress 0x5b3ec */
 - (void)pushButtonEdit:(nullable id)sender {
     [self editStart];
+}
+
+/** @ghidraAddress 0x5b3a8 */
+- (void)pushButtonUpload:(nullable id)sender {
+    if ([self checkEnableUpload]) {
+        [self uploadStart];
+    }
+}
+
+/** @ghidraAddress 0x5d368 */
+- (void)loadListRelease {
+    [self.pFileListView setDelegate:nil];
+    self.pFileListView = nil;
+}
+
+/** @ghidraAddress 0x5e2b4 */
+- (void)popoverPresentationControllerDidDismissPopover:
+    (nullable UIPopoverPresentationController *)popoverPresentationController {
+    [self loadListRelease];
+    [self.controller enableCoverTap];
 }
 
 /** @ghidraAddress 0x5c4fc */
--- Project/MusicDetailViewRpl.m
+++ Project/MusicDetailViewRpl.m
@@ -2,6 +2,8 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+#import "MusicSelectViewController.h"
+
 @implementation MusicDetailViewRpl
 
 /** @ghidraAddress 0x12ad40 */
@@ -12,6 +14,26 @@
 /** @ghidraAddress 0x135630 */
 - (void)pushButtonEdit:(nullable id)sender {
     [self editStart];
+}
+
+/** @ghidraAddress 0x13554c */
+- (void)pushButtonUpload:(nullable id)sender {
+    if ([self checkEnableUpload]) {
+        [self uploadStart];
+    }
+}
+
+/** @ghidraAddress 0x13752c */
+- (void)loadListRelease {
+    [self.pFileListView setDelegate:nil];
+    self.pFileListView = nil;
+}
+
+/** @ghidraAddress 0x1384d4 */
+- (void)popoverPresentationControllerDidDismissPopover:
+    (nullable UIPopoverPresentationController *)popoverPresentationController {
+    [self loadListRelease];
+    [self.controller enableCoverTap];
 }
 
 /** @ghidraAddress 0x1366a0 */
